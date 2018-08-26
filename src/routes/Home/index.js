611bda649bc71a67f7291e2c043afcfcc3b597da
prameetc <prameet.c@gmail.com>
2018-08-26 14:22:48 +0530

    Added Validation and Bootstrap

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -5,12 +5,12 @@ import Select from 'react-select';
 
 // This is a sample data set. 
 const fields = [
-  { name: 'name', type: 'text', placeholder: 'Enter Name' },
-  { name: 'age', type: 'number', placeholder: 'Enter age' },
-  { name: 'email', type: 'email', placeholder: 'Enter Email' },
-  { name: 'employed', type: 'checkbox' },
+  { name: 'Name', type: 'text', placeholder: 'Enter Name'},
+  { name: 'Age', type: 'number', placeholder: 'Enter age' },
+  { name: 'Email', type: 'email', placeholder: 'Enter Email'},
+  { name: 'Employed', type: 'checkbox' },
   {
-    name: 'favouriteColors',
+    name: 'Favourite Colors',
     type: 'select',
     options: [
       { label: 'Red', value: 'red' },
@@ -18,34 +18,50 @@ const fields = [
       { label: 'Green', value: 'green' },
     ],
   },
+  { name: 'Gender', type: 'radio', value: 'Male' },
 ]
 
-// Function to check for validation
-const validate = values => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = "Email is Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email";
-  } else if (!values.password) {
-    errors.password = "Password is Required";
-  } else if (values.password !== values.confirmPassword) {
-    errors.confirmPassword = "Entered passwords doesn't match";
-  }
-  return errors;
-};
+// Regex check
+const required = value => value ? undefined : 'Required'
+const maxLength = max => value =>
+  value && value.length > max ? `Must be ${max} characters or less` : undefined
+const maxLength15 = maxLength(15)
+const minLength5 = maxLength(5);
+const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined
+const minValue = min => value =>
+  value && value < min ? `Must be at least ${min}` : undefined
+const minValue18 = minValue(18)
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+    'Invalid email address' : undefined
 
 // Component to render the field
- const renderField = ({ input, field }) => {
-      const { type, placeholder } = field
-      if (type === 'text' || type === 'email' || type === 'number' || type === 'checkbox') {
-        return <input {...input} placeholder={placeholder} type={type} />
+ const renderField = ({ input, field, meta: { touched, error, warning } }) => {
+   const { type, placeholder, value, name } = field
+   if (type === 'text' || type === 'email' || type === 'number' || type === 'checkbox') {
+        return (
+          <div>
+            <input {...input} className="form-control" placeholder={placeholder} type={type} />
+            {touched && ((error && <span style={{ color: 'red' }}>{error}</span>) || (warning && <span>{warning}</span>))}  
+          </div>
+        )
       } else if (type === 'select') {
         const { options } = field
         return (
-          <Select options={options} isSerchable={options.length > 2 ? true : false} />
+          <div style={{ display: 'block', width: '40%' }} className="text-center">
+            <Select options={options} isSerchable={options.length > 2 ? true : false} />
+          </div>
         )
-      } else {
+      } 
+      else if (type === 'radio') {
+        return (
+          <div>
+            <label className="p-2">{value}</label>
+            <input {...input} type={type} value={value} />
+          </div>
+        )
+      }
+      else {
         return <div>Type not supported.</div>
       }
  }
@@ -58,22 +74,28 @@ function submit(values) {
 class Home extends Component {
   
   render() { 
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine, reset, submitting } = this.props
     return (
-      <div>
-        <h2>React Redux Sample Dynamic Form</h2>
-        <form onSubmit={handleSubmit(submit)}>
-          {fields.map(field => (
+      <div className="container">
+        <div className="form-group w-50">
+          <h2>React Redux Sample Dynamic Form</h2>
+          <form onSubmit={handleSubmit(submit)}>
+            {fields.map(field => (
             <div key={field.name}>
-              <Field
-                name={field.name}
-                component={renderField}
-                field={field}
-              />
+              <label className="font-weight-bold pt-3">{field.name}</label>
+              <div>
+                <Field
+                  name={field.name}
+                  component={renderField}
+                  field={field}
+                  validate={[required, email]}
+                />
+              </div>
             </div>
-          ))}
-          <button type="submit">Submit</button>
-        </form>
+            ))}
+            <button className="btn btn-secondary" type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     )
   }
@@ -81,6 +103,5 @@ class Home extends Component {
 
 export default reduxForm({
   form: "sampleform", 
-  validate,
   destroyOnUnmount: false
 })(Home);
